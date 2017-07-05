65cf60028aa241c6f9b81bd338e35604ca5eee7d
pradeep26stark <pradeeprajaei@gmail.com>
2017-07-05 21:21:10 +0530

    Update compar.java

--- compar.java
+++ compar.java
@@ -1,1 +1,23 @@
-welcome
+import java.util.*;
+class Comparee
+{
+  public static void main(String args[])
+  {
+    Scanner a = new Scanner(System.in);
+    int n,i,fact=1;
+    n = a.nextInt();
+    if(n==0)
+    {
+      System.out.println(1);
+    }
+    else
+    {
+      for(i=1;i<=n;i++)
+      {
+        fact=fact*i;
+      }
+      System.out.println(fact);
+    }
+  }
+}
+   
